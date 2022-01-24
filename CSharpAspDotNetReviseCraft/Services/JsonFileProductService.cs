a8be255eb5aba0471f2023f08fa923263a065c73
mark <markpackham1@gmail.com>
2022-01-24 16:38:10 +0000

    write to json file
    https://youtu.be/oPKq9fNJ6c0?t=563

--- CSharpAspDotNetReviseCraft/Services/JsonFileProductService.cs
+++ CSharpAspDotNetReviseCraft/Services/JsonFileProductService.cs
@@ -36,6 +36,29 @@ namespace CSharpAspDotNetReviseCraft.Services
             var products = GetProducts();
 
             var query = products.First(x => x.Id == productId);
+
+            if (query.Ratings == null)
+            {
+                query.Ratings = new int[] {rating};
+            }
+            else
+            {
+                var ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>({
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true,
+                    }),
+                        products
+                );
+            }
         }
     }
 }
