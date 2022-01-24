d109757bcfe2ca69d4ffbdf79da9a821b7208b9b
mark <markpackham1@gmail.com>
2022-01-24 11:44:44 +0000

    create JseonFileProductService to convert Json to Objects
    https://youtu.be/KZxTOCuTuc0?t=645

---
+++ CSharpAspDotNetReviseCraft/Services/JsonFileProductService.cs
@@ -0,0 +1,34 @@
+﻿using CSharpAspDotNetReviseCraft.Models;
+using System.Text.Json;
+
+namespace CSharpAspDotNetReviseCraft.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        // IEnumerable is stuff you can foreach over/Inumerate, Collection & Array List are its children
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                // take our Json file and turn it into objects that can be put inside an array
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
